5700fc2d8070395bf115fd45ae7e814961ae9ea2
Timo Neske <neske.timo93@gmail.com>
2025-06-29 23:35:21 +0200

    cleaned class from not needed imports

--- src/main/java/power/PowerconsumptionApplication.java
+++ src/main/java/power/PowerconsumptionApplication.java
@@ -3,17 +3,13 @@ package power;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import power.calc.ConsumptionCalculator;
 import power.laufzeit.LaufzeitFileManager;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @EnableScheduling
 @SpringBootApplication
 public class PowerconsumptionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PowerconsumptionApplication.class, args);
 	}
