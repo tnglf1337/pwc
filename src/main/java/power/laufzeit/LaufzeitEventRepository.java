8d726cae9e285dd13e2df2d27e96b3a01eeca81c
Timo Neske <neske.timo93@gmail.com>
2025-06-29 23:36:12 +0200

    added mthod findAll

--- src/main/java/power/laufzeit/LaufzeitEventRepository.java
+++ src/main/java/power/laufzeit/LaufzeitEventRepository.java
@@ -3,13 +3,16 @@ package power.laufzeit;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
+import org.springframework.lang.NonNull;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LaufzeitEventRepository extends CrudRepository<LaufzeitEvent, Integer> {
-	@Query("select stamped_at from laufzeit_event order by stamped_at desc limit 1")
+
+	@NonNull
+	List<LaufzeitEvent> findAll();
+
+	@Query("select stamped_at from laufzeit_event order by stamped_at limit 1")
 	LocalDate getFirstLaufzeit();
 
 	@Query("SELECT * FROM laufzeit_event WHERE stamped_at::date = :date")
