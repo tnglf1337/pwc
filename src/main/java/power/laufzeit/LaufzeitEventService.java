43a07608d3b93d0787c29e5888a6fc12ec05e0ff
Timo Neske <neske.timo93@gmail.com>
2025-06-29 23:37:18 +0200

    added several methods to format dates

--- src/main/java/power/laufzeit/LaufzeitEventService.java
+++ src/main/java/power/laufzeit/LaufzeitEventService.java
@@ -4,14 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import power.DetailsDto;
 import power.calc.ConsumptionCalculator;
 import power.tarif.Tarif;
 import power.tarif.TarifRepository;
+import power.util.Formatter;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.YearMonth;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import static power.util.Formatter.*;
 
 @Service
 public class LaufzeitEventService {
@@ -51,8 +58,23 @@ public class LaufzeitEventService {
 		return laufzeitEventRepository.findByStampedAt(date);
 	}
 
-	public int getLaufzeitSekundenbyDate(LocalDate date) {
+	public int getLaufzeitSekundenByDate(LocalDate date) {
 		List<LaufzeitEvent> events = laufzeitEventRepository.findByStampedAt(date);
+		if (events.isEmpty()) return 0;
+		return getLaufzeitSekunden(events);
+	}
+
+	public int getLaufzeitSekundenByMonth(Month month) {
+		List<LaufzeitEvent> events = laufzeitEventRepository.findAll()
+				.stream()
+				.filter( e -> e.stampedAt().getMonth() == month)
+				.toList();
+		if (events.isEmpty()) return 0;
+		return getLaufzeitSekunden(events);
+	}
+
+	private int getLaufzeitSekunden(List<LaufzeitEvent> events) {
+		if(events.size() == 1) return events.get(0).laufzeit();
 
 		int min = events.get(0).laufzeit();
 		int max = events.get(0).laufzeit();
@@ -61,13 +83,61 @@ public class LaufzeitEventService {
 			int currentLaufzeit = event.laufzeit();
 			if (currentLaufzeit < min) min = event.laufzeit();
 			if (currentLaufzeit > max) max = currentLaufzeit;
+		}
+		return max - min;
+	}
 
+	public List<DetailsDto> computeTableDetailsThisMonth(int monthValue) {
+		List<DetailsDto> l = new LinkedList<>();
+		Tarif currTarif = tarifRepository.findAktuellertatrif();
+
+		YearMonth yearMonth = YearMonth.of(2025, monthValue);
+		int daysInMonth = yearMonth.lengthOfMonth();
+
+		for (int i = 1; i <= daysInMonth; i++) {
+			LocalDate currentDate = LocalDate.of(2025, monthValue, i);
+
+			int laufzeit = getLaufzeitSekundenByDate(currentDate);
+			if (laufzeit == 0) continue;
+
+			double kwh = calculator.kiloWattProStunde((double) laufzeit / 3600);
+			double kosten = currTarif.calculateKwhCost(kwh);
+
+			DetailsDto detail = new DetailsDto(
+					convertLocalDateDE(currentDate),
+					formatSeconds(laufzeit),
+					format(kwh),
+					format(kosten)
+			);
+
+			l.add(detail);
 		}
 
-		System.out.println("min: " + min);
-		System.out.println("max: " + max);
+		Collections.reverse(l);
 
-		return max - min;
+		return l;
+	}
 
+
+	public List<DetailsDto> computeTableDetailsMonthly() {
+		List<DetailsDto> l = new LinkedList<>();
+		Tarif currTarif = tarifRepository.findAktuellertatrif();
+		Month[] months = Month.values();
+
+		for(int i = 1; i <= 12; i++) {
+			Month month = months[i-1];
+			String currentMonth = Formatter.getMonthAsString(month);
+			int laufzeit = getLaufzeitSekundenByMonth(month);
+			if(laufzeit == 0) continue;
+			double kwh = calculator.kiloWattProStunde((double) laufzeit / 3600);
+			double kosten = currTarif.calculateKwhCost(kwh);
+
+			DetailsDto detail = new DetailsDto(currentMonth, formatSeconds(laufzeit), format(kwh), format(kosten));
+			l.add(detail);
+		}
+
+		Collections.reverse(l);
+
+		return l;
 	}
 }
--- src/main/java/power/util/Formatter.java
+++ src/main/java/power/util/Formatter.java
@@ -3,11 +3,11 @@ package power.util;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Formatter {
-
 
 	public static String format(double value) {
 		DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
@@ -20,4 +20,25 @@ public class Formatter {
 		return localDate.format(formatter);
 	}
 
+	public static String formatSeconds(int seconds) {
+		long days = seconds / 86400;
+		seconds %= 86400;
+		long hours = seconds / 3600;
+		seconds %= 3600;
+		long minutes = seconds / 60;
+		seconds %= 60;
+
+		return String.format("%dd %dh %dm %ds", days, hours, minutes, seconds);
+	}
+
+	public static String getCurrentMonthAsString() {
+		Month month = LocalDate.now().getMonth();
+		String monthName = month.getDisplayName(java.time.format.TextStyle.FULL, Locale.GERMAN);
+		return monthName.substring(0, 1).toUpperCase() + monthName.substring(1).toLowerCase();
+	}
+
+	public static String getMonthAsString(Month month) {
+		String monthName = month.getDisplayName(java.time.format.TextStyle.FULL, Locale.GERMAN);
+		return monthName.substring(0, 1).toUpperCase() + monthName.substring(1).toLowerCase();
+	}
 }
